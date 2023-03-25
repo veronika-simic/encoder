ba482bac6f22fbd0e7a75fca90396b7573216c1a
veronika-simic <veronikasimic21@gmail.com>
2023-03-25 18:09:31 +0100

    refractored

--- encoder/encoder.js
+++ encoder/encoder.js
@@ -1,26 +1,19 @@
-function encoder(string) {
+function encoder(str) {
   let count = 1;
-  let encodedString = [];
-  string.split("").reduce((accumulator, currentValue, index, arr) => {
-    if (accumulator === currentValue) {
-      count += 1;
-      if (index === arr.length - 1) {
-        encodedString.push(accumulator, count);
-      }
+  const encodedString = str.split("").reduce((acc, curr, index, array) => {
+    if (curr === array[index + 1]) {
+      count++;
+      return acc;
     } else {
-      encodedString.push(accumulator, count);
+      acc.push(curr, count);
       count = 1;
-      if (index === arr.length - 1) {
-        encodedString.push(currentValue, count);
-      }
+      return acc;
     }
-    return currentValue;
-  });
+  }, []);
+
   console.log(encodedString.join(""));
 }
 
 encoder("XXXYYYYZZQQX");
-console.log("-----------------------------------");
 encoder("XYYYYZZQQ");
-console.log("-----------------------------------");
 encoder("XXXYYYYZZQQXX");

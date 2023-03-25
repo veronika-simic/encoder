6e08ec306ab40509118803d5ae1c51026d6ab328
veronika-simic <veronikasimic21@gmail.com>
2023-03-25 18:43:36 +0100

    fix problem with import

--- server/app.js
+++ server/app.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const login = require('../login')
-
+const login = require('./login')
 app.use(express.json())
 
 app.post('/login', login)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 

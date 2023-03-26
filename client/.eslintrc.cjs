2bd9932cc2a7bbdadf7ab1accb52ba5c4ba537b4
veronika-simic <veronikasimic21@gmail.com>
2023-03-26 12:36:05 +0200

    setup tailwind

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -9,15 +9,26 @@ module.exports = {
     'airbnb/hooks',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
     'plugin:prettier/recommended',
   ],
-  overrides: [],
+  overrides: [
+    {
+      files: ['*.html', '*.blade.php'],
+      parser: '@angular-eslint/template-parser',
+    },
+    {
+      files: ['*.ts', '*.tsx', '*.js'],
+      parser: '@typescript-eslint/parser',
+    },
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
+    extraFileExtensions: ['.html'],
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {},

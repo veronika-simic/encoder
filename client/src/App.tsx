fb2a439422011aee70b466a3a76f25aae76aaa8a
veronika-simic <veronikasimic21@gmail.com>
2023-03-26 15:04:09 +0200

    Setup initial state of application
    - add pages
    - install react-router and react-router-dom
    - add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import EncodePage from './pages/Encode/Encode';
+import LoginPage from './pages/Login/LoginPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/encode" element={<EncodePage />} />
+    </>
+  )
+);
+
 function App() {
-  return <div />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/pages/Encode/Encode.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EncodePage() {
+  return <h1>Encode PAge</h1>;
+}

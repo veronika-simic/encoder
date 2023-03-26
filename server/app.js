5e0505223b861d24b777d9fac41f7cb16cf9b914
veronika-simic <veronikasimic21@gmail.com>
2023-03-26 19:39:18 +0200

    install swagger

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
+const app = express();
+const port = 3000;
+const login = require("./login");
+app.use(express.json());
 
-const login = require('./login')
-app.use(express.json())
+app.post("/login", login);
 
-app.post('/login', login)
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ server/encoder.js
@@ -0,0 +1,15 @@
+function encoder(str) {
+  let count = 1;
+  const encodedString = str.split("").reduce((acc, curr, index, array) => {
+    if (curr === array[index + 1]) {
+      count++;
+      return acc;
+    } else {
+      acc.push(curr, count);
+      count = 1;
+      return acc;
+    }
+  }, []);
+
+  return encodedString.join("");
+}

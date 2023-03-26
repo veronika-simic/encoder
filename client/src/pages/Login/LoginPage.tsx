04ca489a76004e3d2185ed0f5110dfcf2bc4eab6
veronika-simic <veronikasimic21@gmail.com>
2023-03-26 15:38:13 +0200

    add universal styles for both pages

--- client/src/pages/Login/LoginPage.tsx
+++ client/src/pages/Login/LoginPage.tsx
@@ -1,5 +1,17 @@
+import { Typography, TextField, Button } from '@mui/material';
 import React from 'react';
 
 export default function LoginPage() {
-  return <h1>Login PAge</h1>;
+  return (
+    <>
+      <h1 className="text-6xl">LOGIN.</h1>
+      <Typography>Welcome Back,</Typography>
+      <Typography>Sign in to continue</Typography>
+      <TextField id="email" label="Email" variant="standard" />
+      <TextField id="password" label="Password" variant="standard" />
+      <Typography>Forgot password?</Typography>
+      <Button variant="outlined">Log in</Button>
+      <Typography>New user? Sign up</Typography>
+    </>
+  );
 }

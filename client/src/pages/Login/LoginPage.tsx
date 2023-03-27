e3a10344ac3b49891074e735b6b4ed7d043d6c52
veronika-simic <veronikasimic21@gmail.com>
2023-03-27 09:25:24 +0200

    validation for email and password fields

--- client/src/pages/Login/LoginPage.tsx
+++ client/src/pages/Login/LoginPage.tsx
@@ -1,7 +1,15 @@
 import { TextField, Button, FormControl, FormHelperText } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleEmailChange = () => {};
+
+  const handlePasswordChange = () => {};
   return (
     <FormControl>
       <h1 className="mb-6 text-6xl font-bold">LOGIN.</h1>
@@ -13,6 +21,11 @@ export default function LoginPage() {
         label="Email"
         variant="standard"
         fullWidth
+        required
+        value={email}
+        onChange={handleEmailChange}
+        error={Boolean(emailError)}
+        helperText={emailError}
         sx={{ display: 'block', my: '2rem' }}
         inputProps={{
           style: { color: 'white' },
@@ -27,6 +40,12 @@ export default function LoginPage() {
         label="Password"
         variant="standard"
         fullWidth
+        required
+        autoComplete="current-password"
+        value={password}
+        onChange={handlePasswordChange}
+        error={Boolean(passwordError)}
+        helperText={passwordError}
         sx={{ display: 'block', mt: '2rem', mb: '1rem' }}
         inputProps={{
           style: { color: 'white' },
